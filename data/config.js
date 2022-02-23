001f4a85397a61e91696c47a703f8be7597cd8c5
Danilo Isamu Inafuku <dan.inafuku@hotmail.com>
2022-02-23 14:51:37 -0300

    teste true

--- data/config.js
+++ data/config.js
@@ -1,5 +1,5 @@
 
-var teste = false;
+var teste = true;
 module.exports.teste = teste;
 module.exports.token = teste ? "OTI4MjM0Mjg0OTI0NDIwMDk2.YdVziw.1Va_gnrO3uFWfVvPcDAPu8WQR6U" : "OTI4MjMxMzA2Njc5MTU2NzY2.YdVwxQ.ADmpLCQ79Y_MZnWlu3VN6J8c0BE";
 module.exports.prefixes = teste ? ["test, "] : ["#"];
